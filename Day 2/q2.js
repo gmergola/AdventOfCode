dcad01da47f0601e338bd6a8d3e63cee7f80ee28
genna <genna@get-carrot.com>
2022-12-02 20:40:40 -0500

    day 2 question 2

--- Day 2/q2.js
+++ Day 2/q2.js
@@ -1,8 +1,11 @@
-
 /*
 A & X = rock 1
 B & Y = paper 2
 C & Z = scissors 3
+
+X means you need to lose 0
+Y means you need to end the round in a draw 3
+Z means you need to win 6
 
 loss = 0
 draw = 3
@@ -15,8 +18,8 @@ const data = fs.readFileSync("data.txt", {encoding: "utf-8"})
     .split("\n")
     .map((x) => x)
 
-function qOne(){
-    const game = {"A Y": 8, "A X": 4, "A Z": 3, "B X": 1, "B Y": 5, "B Z": 9, "C X": 7, "C Y": 2, "C Z": 6 }
+function qTwo(){
+    const game = {"A Y": 4, "A X": 3, "A Z": 8, "B X": 1, "B Y": 5, "B Z": 9, "C X": 2, "C Y": 6, "C Z": 7 }
     let score = 0
 
     for(let match of data){
@@ -26,4 +29,4 @@ function qOne(){
     return score
 }
 
-console.log(qOne()) // 12458
+console.log(qTwo())
